a6908342c08e6cf7058fa4df7d39fea857c30e3c
Felix Li <felix@FelixPro.local>
2020-06-07 01:15:59 +0800

    Add login page

--- src/components/App.js
+++ src/components/App.js
@@ -10,6 +10,7 @@ import NewQuestion from "./NewQuestion";
 import Leaderboard from "./Leaderboard";
 import Question from "./Question";
 import Loading from "./Loading";
+import Login from "./Login";
 
 class App extends Component {
   componentDidMount() {
@@ -31,6 +32,7 @@ class App extends Component {
                 <Route path="/add" component={NewQuestion} />
                 <Route path="/leaderboard" component={Leaderboard} />
                 <Route path="/questions/:id" component={Question} />
+                <Route path="/login" component={Login} />
               </div>
             </div>
           </div>
---
+++ src/components/Login.js
@@ -0,0 +1,70 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { setAuthedUser } from "../actions/authedUser";
+import { withRouter } from "react-router-dom";
+
+export class Login extends Component {
+  state = {
+    selectedUser: null,
+  };
+  updateSelectedUser = (event) => {
+    event.preventDefault();
+    this.setState({
+      selectedUser: event.target.value,
+    });
+  };
+  loginUser = () => {
+    const { selectedUser } = this.state;
+    const { setAuthedUser, history } = this.props;
+
+    setAuthedUser(selectedUser);
+    history.push("/");
+  };
+  render() {
+    const { users } = this.props;
+    const { selectedUser } = this.state;
+    return (
+      <div className="card card-login">
+        <div className="card-content has-text-centered has-background-primary hero">
+          <h3 className="title">Welcome to Would You Rather</h3>
+          <h4 className="subtitle">Please login to continue</h4>
+        </div>
+        <div className="card-content has-text-centered">
+          <div className="select is-fullwidth is-primary">
+            <select
+              value={selectedUser}
+              placeholder="Select a user"
+              onChange={this.updateSelectedUser}
+            >
+              {Object.values(users).map((user) => (
+                <option value={user.id} key={user.id}>
+                  {user.name}
+                </option>
+              ))}
+            </select>
+          </div>
+          <button
+            className="button is-primary is-fullwidth"
+            onClick={this.loginUser}
+          >
+            Log in
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
+
+function mapStateToProps({ users }) {
+  return { users };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    setAuthedUser: (id) => {
+      dispatch(setAuthedUser(id));
+    },
+  };
+}
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Login));
