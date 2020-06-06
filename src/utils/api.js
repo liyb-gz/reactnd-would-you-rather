fa2ac94b0a5d84e07e0fdb7d4d26c0ce1315f679
Felix Li <felix@FelixPro.local>
2020-06-06 17:29:49 +0800

    bug fix: call get users and questions functions in promose

--- src/utils/api.js
+++ src/utils/api.js
@@ -6,16 +6,16 @@ import {
 } from "./_DATA";
 
 export function getInitialData() {
-    return Promise.all([_getUsers, _getQuestions]).then([users, questions] => ({
-        users,
-        questions
-    }))
+  return Promise.all([
+    _getUsers(),
+    _getQuestions(),
+  ]).then(([users, questions]) => ({ users, questions }));
 }
 
 export function saveQuestion(question) {
-    return _saveQuestion(question)
+  return _saveQuestion(question);
 }
 
 export function saveQuestionAnswer(info) {
-    return _saveQuestionAnswer(info)
+  return _saveQuestionAnswer(info);
 }
