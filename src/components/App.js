04155bb2b62723c9456b5e1c52e30bea49708dde
Felix Li <felix@FelixPro.local>
2020-06-06 15:58:40 +0800

    Add home page style

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,22 @@
 import React from "react";
+import { Route } from "react-router-dom";
+
 import Nav from "./Nav";
+import Home from "./Home";
 
 function App() {
   return (
     <div className="App">
       <Nav />
-      Hello World
+      <section class="section wyr-main">
+        <div class="container">
+          <div class="columns is-centered">
+            <div class="column is-two-thirds-tablet is-half-desktop">
+              <Route path="/" exact component={Home} />
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
---
+++ src/components/Home.js
@@ -0,0 +1,86 @@
+import React, { Component, Fragment } from "react";
+
+export class Home extends Component {
+  render() {
+    return (
+      <Fragment>
+        <div className="tabs">
+          <ul>
+            <li className="is-active">
+              <a>Unanswered Questions</a>
+            </li>
+            <li>
+              <a>Answered Questions</a>
+            </li>
+          </ul>
+        </div>
+        <div className="card card-question">
+          <header className="card-header has-background-primary">
+            <p className="card-header-title">Felix Li asks:</p>
+          </header>
+          <div className="card-content">
+            <article className="media">
+              <figure className="media-left">
+                <p className="image is-64x64">
+                  <img
+                    className="is-rounded"
+                    src="https://i.pravatar.cc/300?img=1"
+                    alt="User portrait"
+                  />
+                </p>
+              </figure>
+              <div className="media-content">
+                <div className="content">
+                  <p>
+                    <strong>Would you rather: </strong>
+                    <br />
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit...
+                  </p>
+                </div>
+              </div>
+            </article>
+          </div>
+          <footer className="card-footer">
+            <a href="#" className="card-footer-item">
+              View
+            </a>
+          </footer>
+        </div>
+        <div className="card card-question">
+          <header className="card-header has-background-primary">
+            <p className="card-header-title">Felix Li asks:</p>
+          </header>
+          <div className="card-content">
+            <article className="media">
+              <figure className="media-left">
+                <p className="image is-128x128">
+                  <img
+                    className="is-rounded"
+                    src="https://i.pravatar.cc/300?img=1"
+                    alt="User portrait"
+                  />
+                </p>
+              </figure>
+              <div className="media-content">
+                <div className="content">
+                  <p>
+                    <strong>Would you rather: </strong>
+                    <br />
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit...
+                  </p>
+                </div>
+              </div>
+            </article>
+          </div>
+          <footer className="card-footer">
+            <a href="#" className="card-footer-item">
+              View
+            </a>
+          </footer>
+        </div>
+      </Fragment>
+    );
+  }
+}
+
+export default Home;
