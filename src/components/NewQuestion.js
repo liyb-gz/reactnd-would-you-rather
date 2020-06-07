e25c6400ade5fff8e510b939582abd614f6ec9c3
Felix Li <felix@FelixPro.local>
2020-06-07 12:03:20 +0800

    Make new question input to be controlled

--- src/components/NewQuestion.js
+++ src/components/NewQuestion.js
@@ -1,7 +1,19 @@
 import React, { Component } from "react";
 
 export class NewQuestion extends Component {
+  state = {
+    option1: "",
+    option2: "",
+  };
+
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
+
   render() {
+    const { option1, option2 } = this.state;
     return (
       <div className="card card-question">
         <header className="card-header has-background-primary">
@@ -13,22 +25,39 @@ export class NewQuestion extends Component {
               <h3>Would you rather:</h3>
               <div className="field">
                 <div className="control">
-                  <input className="input" type="text" placeholder="Option 1" />
+                  <input
+                    className="input"
+                    value={option1}
+                    onChange={this.handleChange}
+                    name="option1"
+                    type="text"
+                    placeholder="Option 1"
+                  />
                 </div>
               </div>
               <p>or</p>
               <div className="field">
                 <div className="control">
-                  <input className="input" type="text" placeholder="Option 2" />
+                  <input
+                    className="input"
+                    value={option2}
+                    onChange={this.handleChange}
+                    name="option2"
+                    type="text"
+                    placeholder="Option 2"
+                  />
                 </div>
               </div>
             </form>
           </div>
         </div>
         <footer className="card-footer">
-          <a href="#" className="card-footer-item">
+          <button
+            className="card-footer-item button is-white"
+            disabled={!(option1 || option2)}
+          >
             Submit
-          </a>
+          </button>
         </footer>
       </div>
     );
